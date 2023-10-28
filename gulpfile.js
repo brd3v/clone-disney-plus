3f39821de974a5b1c8b5d59f9a16b22607658681
brd3v <bnsiq2015@gmail.com>
2023-10-28 17:41:14 -0300

    alterações

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,23 @@
 
 const gulp = require('gulp')
 const sass= require('gulp-sass')(require('sass'))
+const imagemin = require('gulp-imagemin')
 //css
 function styles(){
     return gulp.src('./src/styles/main.scss')
         .pipe(sass({outputStyle: 'compressed'}))
         .pipe(gulp.dest('./dist/css'))
 }
-exports.default = styles
+
+//imagemin
+function images(){
+    return gulp.src('./src/imagens/**/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('./dist/images'));
+}
+
+//exportação imagemin e compressao do sass
+exports.default = gulp.parallel(styles, images)
 
 
 //watch
